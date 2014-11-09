57ae7b8dffa6815ebf20016b74439cf5d739e581
Administrator <admin@local.host>
2014-11-09 00:12:35 +0000

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,113 @@
-googlemaptiler
+Google Image Map Tiler
 ==============
 
 Creates image tiles for Google Maps
+
+
+Map tile generator for Google Maps
+
+Java based version of implementation at
+
+http://blog.mikecouturier.com/2011/07/create-zoomable-images-using-google.html
+by Mike Couturier
+
+To use run
+
+```
+TilesGenerator [1-15] [filename] 
+
+```
+where [1-15] signifies the zoom level and the [filename] is the image you want to segment.
+
+Then use in your webpage
+
+```
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Image map types</title>
+    <style>
+        html, body, #map-canvas {
+        height: 100%;
+        width: 100%;
+        margin: 0px;
+        padding: 0px
+        }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <script>
+var moonTypeOptions = {
+  getTileUrl: function(coord, zoom) {
+      var normalizedCoord = getNormalizedCoord(coord, zoom);
+      if (!normalizedCoord) {
+        return null;
+      }
+      var bound = Math.pow(2, zoom);
+       return 'file:///[filename]' +
+          '/tile_' + zoom + '_' + normalizedCoord.x + '_' +
+          (normalizedCoord.y    ) + '.png';
+  },
+  tileSize: new google.maps.Size(256, 256),
+  maxZoom: 3,
+  minZoom: 0,
+  radius: 1738000,
+  name: 'example'
+};
+
+var moonMapType = new google.maps.ImageMapType(moonTypeOptions);
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(0, 0);
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 1,
+    streetViewControl: false,
+    mapTypeControlOptions: {
+      mapTypeIds: ['example']
+    }
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  map.mapTypes.set('example', moonMapType);
+  map.setMapTypeId('example');
+}
+
+// Normalizes the coords that tiles repeat across the x axis (horizontally)
+// like the standard Google map tiles.
+function getNormalizedCoord(coord, zoom) {
+  var y = coord.y;
+  var x = coord.x;
+
+  // tile range in one direction range is dependent on zoom level
+  // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+  var tileRange = 1 << zoom;
+
+  // don't repeat across y-axis (vertically)
+  if (y < 0 || y >= tileRange) {
+    return null;
+  }
+
+  // don't repeat across x-axis
+  if (x < 0 || x >= tileRange) {
+    //x = (x % tileRange + tileRange) % tileRange;
+    return null;
+  }
+
+  return {
+    x: x,
+    y: y
+  };
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
+</head>
+<body>
+<div id="map-canvas"></div>
+</body>
+</html>
+
+```
+
---
+++ src/main/java/com/devl/development/googlemaptiler/Program.java
@@ -0,0 +1,123 @@
+package com.devl.development.googlemaptiler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Copyright 2014 Dev Lakhani
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Program {
+  static int TILE_SIZE = 256;
+  static String TILE_FILENAME = "tile_%d_%d_%d.png";
+  static String OUTPUT_DIR = "./output";
+
+  public static void main(String[] args) {
+    int level = Integer.parseInt(args[0]);
+
+    if (args.length != 2 || level < 1 || level > 15 || !new File(args[1]).exists()) {
+      System.err.println("usage: \"TilesGenerator [1-15] [filename]\"");
+      return;
+    }
+
+    try {
+      int mapWidth = GetMapWidth(level);
+
+      if (!new File(OUTPUT_DIR).exists()) {
+        new File(OUTPUT_DIR).mkdir();
+      }
+
+      BufferedImage original = null;
+      try {
+        original = ImageIO.read(new File(args[1]));
+
+        SplitTilesRecursive(original, level);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  static void SplitTilesRecursive(BufferedImage original, int level) {
+    int mapWidth = GetMapWidth(level);
+    int tilesOnSide = mapWidth / TILE_SIZE;
+    BufferedImage resized = ResizeImage(original, mapWidth * 2, mapWidth);
+
+    for (int x = 0; x < tilesOnSide * 2; x++)
+      for (int y = 0; y < tilesOnSide; y++)
+        CropAndSaveTile(resized, x, y, level);
+
+    if (level > 0)
+      SplitTilesRecursive(original, level - 1);
+  }
+
+  private static int GetMapWidth(int level) {
+    return TILE_SIZE * (int) Math.pow(2, level);
+  }
+
+  private static void CropAndSaveTile(BufferedImage image, int x, int y, int level) {
+    Rectangle cropArea = new Rectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+
+    BufferedImage clone = deepCopy(image);
+    BufferedImage cropped = cropImage(clone, cropArea);
+    String filename = String.format(TILE_FILENAME, level, x, y);
+    try {
+      ImageIO.write(cropped, "png", new File(OUTPUT_DIR + "/" + filename));
+      System.err.println("Done " + filename);
+      cropped = null;
+      clone = null;
+    } catch (IOException e) {
+
+      e.printStackTrace();
+    }
+  }
+
+  private static BufferedImage cropImage(BufferedImage src, Rectangle rect) {
+    BufferedImage dest = src.getSubimage(rect.x, rect.y, rect.width, rect.height);
+    return dest;
+  }
+
+  private static BufferedImage ResizeImage(BufferedImage toResize, int width, int height) {
+    BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g = resizedImage.createGraphics();
+    //improve resize quality
+    g.setComposite(AlphaComposite.Src);
+    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+        RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g.drawImage(toResize, 0, 0, width, height, null);
+    g.dispose();
+    return resizedImage;
+
+  }
+
+  private static BufferedImage deepCopy(BufferedImage bi) {
+    BufferedImage copyOfImage =
+        new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
+    Graphics g = copyOfImage.createGraphics();
+    g.drawImage(bi, 0, 0, null);
+    return copyOfImage;
+  }
+
+}
